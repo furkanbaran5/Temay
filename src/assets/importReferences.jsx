01dcebdbe29076cb81993a16e7ef4b4c39ea76e5
Furkan Baran <furkanbarann5@gmail.com>
2025-03-14 00:55:28 +0300

    frk

--- src/assets/importReferences.jsx
+++ src/assets/importReferences.jsx
@@ -1,5 +1,5 @@
 import sponsor1 from "./Logo1.png";
-import sponsor2 from "./TEMAY.png";
+import sponsor2 from "./TEMAY_beyaz.png";
 
 const references = [
     sponsor1, sponsor2, sponsor2, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1, sponsor1
